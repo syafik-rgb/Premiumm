45c670a44f569e750f096a79f1545677c68eba3a
Kontol <83557978+syafik-rgb@users.noreply.github.com>
2021-06-02 04:36:30 +0000

    Update Premium.py

--- Premium.py
+++ Premium.py
@@ -1,4 +1,4 @@
-# uncompyle6 version 3.7.4
+# uncompyle6 version 3.6.7
 # Python bytecode 2.7 (62211)
 # Decompiled from: Python 2.7.18 (default, Mar 20 2021, 14:58:25) 
 # [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
